070556328f36f3d8b0cb782dd72de6438ae82c4f
Mohit kumar <kmohit4166@gmail.com>
2026-09-21 23:57:08 +0530

    update code

--- models/user.js
+++ models/user.js
@@ -1,5 +1,6 @@
 const { Schema, model } = require('mongoose');
 const { randomBytes, createHmac } = require('crypto');
+const { createTokenForUser } = require('../services/authentication');
 
 const userSchema = new Schema({
     fullName: {
@@ -41,7 +42,7 @@ userSchema.pre("save", function () {
     this.password = hashedPassword;
 });
 
-userSchema.static('matchPassword', async function(email, password) {
+userSchema.static('matchPasswordAndGenerateToken', async function(email, password) {
     const user = await this.findOne({email});
     if(!user) throw new Error('user not found!') ;
 
@@ -51,7 +52,9 @@ userSchema.static('matchPassword', async function(email, password) {
     const userProvidedHash = createHmac("sha256", salt).update(password).digest("hex"); 
 
     if (hashedPassword !== userProvidedHash)throw new Error('Incorrect password') ;
-    return user;
+    
+    const token = createTokenForUser(user);
+    return token;
 });
 
 const User = model("user", userSchema);
--- routes/user.js
+++ routes/user.js
@@ -12,9 +12,13 @@ router.get('/signup', (req, res) => {
 
 router.post('/signin', async(req, res) => {
     const { email, password } = req.body;
-    const user = await User.matchPassword(email, password);
+    try {
+        const token = await User.matchPasswordAndGenerateToken(email, password);
 
-    return res.redirect("/");
+    return res.cookie("token", token).redirect("/");
+    } catch (error) {
+        return res.render("signin", {error: 'Incorrect password or email',});
+    }
 });
 
 router.post('/signup', async(req, res) => {
