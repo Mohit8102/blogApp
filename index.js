04e60d45d36b03f014f44d2d5f687959d33f6031
Mohit kumar <kmohit4166@gmail.com>
2026-09-24 23:03:54 +0530

    update code

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const blogRoute = require('./routes/blog');
 const { checkForAuthenticationCookie } = require('./middlewares/authentication');
 
 const app = express();
-const PORT = 8000;
+const PORT = 8001;
 
 mongoose
     .connect('mongodb://127.0.0.1:27017/blogify')
